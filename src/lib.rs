365b8196222a9fd9908d18f0a7135bad3d44ff57
Andrew Walbran <qwandor@google.com>
2025-11-17 15:45:41 +0000

    Start with a copy of the arm-sysregs crate from RF-A.
    This was copied from
    https://review.trustedfirmware.org/plugins/gitiles/RF-A/rusted-firmware-a/+/e88cdf95f3c14f30d177f6f62173c7fc55f574a4.
    
    All contributors have agreed to license this under the MIT or Apache 2.0
    licenses, rather than the BSD 3 Clause license previously used in RF-A.
    
    Change-Id: Idf17fd2e4486b553f8482ab7d373f596e213f592
    Signed-off-by: Andrew Walbran <qwandor@google.com>

--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: Copyright The arm-sysregs Contributors.
-// SPDX-License-Identifier: MIT OR Apache-2.0
-
-#![no_std]
-#![doc = include_str!("../README.md")]
-#![deny(clippy::undocumented_unsafe_blocks)]
-#![deny(unsafe_op_in_unsafe_fn)]
