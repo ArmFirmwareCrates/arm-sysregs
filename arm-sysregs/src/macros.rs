60195814438c2c28a08838fe90b578aca6e3755d
Andrew Walbran <qwandor@google.com>
2025-11-17 15:45:41 +0000

    Start with a copy of the arm-sysregs crate from RF-A.
    This was copied from
    https://review.trustedfirmware.org/plugins/gitiles/RF-A/rusted-firmware-a/+/59653d6ab18f564d570a5799c8ca6825b6a7767c.
    
    Change-Id: Idf17fd2e4486b553f8482ab7d373f596e213f592
    Signed-off-by: Andrew Walbran <qwandor@google.com>

---
+++ arm-sysregs/src/macros.rs
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: Copyright The arm-sysregs Contributors.
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+/// Generates public functions named `read_$sysreg` and `write_$sysreg` to read or write
+/// (respectively) a value of type `$type` from/to the system register `$sysreg`.
+///
+/// `safe_read` and `safe_write` should only be specified for system registers which are indeed safe
+/// to read from or write any value to.
+#[macro_export]
+macro_rules! read_write_sysreg {
+    ($sysreg:ident $(: $asm_sysreg:ident)?, $type:ty $(: $bitflags_type:ty)?, safe_read, safe_write $(, $fake_sysregs:expr)?) => {
+        $crate::read_sysreg!($sysreg $(: $asm_sysreg)?, $type $(: $bitflags_type)?, safe $(, $fake_sysregs)?);
+        $crate::write_sysreg!($sysreg $(: $asm_sysreg)?, $type $(: $bitflags_type)?, safe $(, $fake_sysregs)?);
+    };
+    (
+        $(#[$attributes:meta])*
+        $sysreg:ident $(: $asm_sysreg:ident)?, $type:ty $(: $bitflags_type:ty)?, safe_read $(, $fake_sysregs:expr)?
+    ) => {
+        $crate::read_sysreg!($sysreg $(: $asm_sysreg)?, $type $(: $bitflags_type)?, safe $(, $fake_sysregs)?);
+        $crate::write_sysreg! {
+            $(#[$attributes])*
+            $sysreg $(: $asm_sysreg)?, $type $(: $bitflags_type)? $(, $fake_sysregs)?
+        }
+    };
+}
