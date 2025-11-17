9d5328f6811cafd5efa090817acb3c44d4e36b83
Andrew Walbran <qwandor@google.com>
2025-11-17 16:40:42 +0000

    WIP: use generated arm-sysregs
    Change-Id: I100de2181712a98ee41eae1636867cae69897d67

--- arm-sysregs/src/manual.rs
+++ arm-sysregs/src/manual.rs
@@ -3,48 +3,66 @@
 
 //! Manually implemented methods for system register types.
 
-use crate::{Esr, IdAa64mmfr1El1, IdAa64mmfr2El1, IdAa64mmfr3El1, MpidrEl1, Spsr, read_mpidr_el1};
+use crate::{
+    EsrEl1, EsrEl2, EsrEl3, IdAa64mmfr1El1, IdAa64mmfr2El1, IdAa64mmfr3El1, MpidrEl1, SpsrEl1,
+    SpsrEl2, SpsrEl3, read_mpidr_el1,
+};
 use core::fmt::{self, Debug, Formatter};
 
-impl Esr {
+impl EsrEl1 {
     /// Mask for the parts of an ESR value containing the opcode.
     pub const ISS_SYSREG_OPCODE_MASK: Self = Self::from_bits_retain(0x003f_fc1e);
 }
 
-impl Debug for Esr {
+impl Debug for EsrEl1 {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "Esr({:#x})", self.0)
+        write!(f, "EsrEl1({:#x})", self.0)
+    }
+}
+
+impl EsrEl2 {
+    /// Mask for the parts of an ESR value containing the opcode.
+    pub const ISS_SYSREG_OPCODE_MASK: Self = Self::from_bits_retain(0x003f_fc1e);
+}
+
+impl Debug for EsrEl2 {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "EsrEl2({:#x})", self.0)
+    }
+}
+
+impl EsrEl3 {
+    /// Mask for the parts of an ESR value containing the opcode.
+    pub const ISS_SYSREG_OPCODE_MASK: Self = Self::from_bits_retain(0x003f_fc1e);
+}
+
+impl Debug for EsrEl3 {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "EsrEl3({:#x})", self.0)
     }
 }
 
 impl IdAa64mmfr1El1 {
-    const VH_SHIFT: u64 = 8;
-    const VH_MASK: u64 = 0b1111;
-    const VH_SUPPORTED: u64 = 0b0001;
-
-    const HCX_SHIFT: u64 = 40;
-    const HCX_MASK: u64 = 0b1111;
-    const HCX_SUPPORTED: u64 = 0b0001;
+    const VH_SUPPORTED: u8 = 0b0001;
+    const HCX_SUPPORTED: u8 = 0b0001;
 
     /// Indicates presence of FEAT_VHE.
     pub fn is_feat_vhe_present(self) -> bool {
-        (self.bits() >> Self::VH_SHIFT) & Self::VH_MASK >= Self::VH_SUPPORTED
+        self.vh() >= Self::VH_SUPPORTED
     }
 
     /// Indicates presence of FEAT_HCX.
     pub fn is_feat_hcx_present(self) -> bool {
-        (self.bits() >> Self::HCX_SHIFT) & Self::HCX_MASK >= Self::HCX_SUPPORTED
+        self.hcx() >= Self::HCX_SUPPORTED
     }
 }
 
 impl IdAa64mmfr2El1 {
-    const CCIDX_SHIFT: u64 = 20;
-    const CCIDX_MASK: u64 = 0b1111;
-    const CCIDX_64_BIT: u64 = 0b0001;
+    const CCIDX_64_BIT: u8 = 0b0001;
 
     /// Checks whether 64-bit format is implemented for all levels of the CCSIDR_EL1.
     pub fn has_64_bit_ccsidr_el1(self) -> bool {
-        (self.bits() >> Self::CCIDX_SHIFT) & Self::CCIDX_MASK == Self::CCIDX_64_BIT
+        self.ccidx() == Self::CCIDX_64_BIT
     }
 }
 
@@ -73,7 +91,17 @@ impl MpidrEl1 {
     }
 }
 
-impl Spsr {
+impl SpsrEl1 {
+    /// All of the N, Z, C and V bits.
+    pub const NZCV: Self = Self::V.union(Self::C).union(Self::Z).union(Self::N);
+}
+
+impl SpsrEl2 {
+    /// All of the N, Z, C and V bits.
+    pub const NZCV: Self = Self::V.union(Self::C).union(Self::Z).union(Self::N);
+}
+
+impl SpsrEl3 {
     /// AArch64 execution state, EL0.
     pub const M_AARCH64_EL0: Self = Self::from_bits_retain(0b00000);
     /// AArch64 execution state, EL1 with SP_EL0.
@@ -95,27 +123,7 @@ impl Spsr {
     pub const SP_ELX: Self = Self::from_bits_retain(1);
 
     /// All of the N, Z, C and V bits.
-    pub const NZCV: Self = Spsr::V.union(Spsr::C).union(Spsr::Z).union(Spsr::N);
-
-    /// Returns the value of the EL field.
-    pub const fn exception_level(self) -> ExceptionLevel {
-        match (self.bits() >> Self::EL_SHIFT) & Self::EL_MASK {
-            0 => ExceptionLevel::El0,
-            1 => ExceptionLevel::El1,
-            2 => ExceptionLevel::El2,
-            3 => ExceptionLevel::El3,
-            _ => unreachable!(),
-        }
-    }
-
-    /// Returns the value of the SP field.
-    pub const fn stack_pointer(self) -> StackPointer {
-        match self.bits() & Self::SP_MASK {
-            0 => StackPointer::El0,
-            1 => StackPointer::ElX,
-            _ => unreachable!(),
-        }
-    }
+    pub const NZCV: Self = Self::V.union(Self::C).union(Self::Z).union(Self::N);
 }
 
 /// Cache type enum.
@@ -213,19 +221,51 @@ mod tests {
     }
 
     #[test]
-    fn debug_spsr() {
-        assert_eq!(format!("{:?}", Spsr::empty()), "Spsr(0x0)");
-        assert_eq!(format!("{:?}", Spsr::NZCV), "Spsr(V | C | Z | N)");
-        assert_eq!(format!("{:?}", Spsr::M_AARCH64_EL3H), "Spsr(0xd)");
+    fn debug_spsr_el1() {
+        assert_eq!(format!("{:?}", SpsrEl1::empty()), "SpsrEl1(0x0)");
+        assert_eq!(format!("{:?}", SpsrEl1::NZCV), "SpsrEl1(V | C | Z | N)");
     }
 
     #[test]
-    fn debug_esr() {
-        assert_eq!(format!("{:?}", Esr::empty()), "Esr(0x0)");
-        assert_eq!(format!("{:?}", Esr::IL), "Esr(0x2000000)");
+    fn debug_spsr_el2() {
+        assert_eq!(format!("{:?}", SpsrEl2::empty()), "SpsrEl2(0x0)");
+        assert_eq!(format!("{:?}", SpsrEl2::NZCV), "SpsrEl2(V | C | Z | N)");
+    }
+
+    #[test]
+    fn debug_spsr_el3() {
+        assert_eq!(format!("{:?}", SpsrEl3::empty()), "SpsrEl3(0x0)");
+        assert_eq!(format!("{:?}", SpsrEl3::NZCV), "SpsrEl3(V | C | Z | N)");
+        assert_eq!(format!("{:?}", SpsrEl3::M_AARCH64_EL3H), "SpsrEl3(0xd)");
+    }
+
+    #[test]
+    fn debug_esr_el1() {
+        assert_eq!(format!("{:?}", EsrEl1::empty()), "EsrEl1(0x0)");
+        assert_eq!(format!("{:?}", EsrEl1::IL), "EsrEl1(0x2000000)");
         assert_eq!(
-            format!("{:?}", Esr::ISS_SYSREG_OPCODE_MASK),
-            "Esr(0x3ffc1e)"
+            format!("{:?}", EsrEl1::ISS_SYSREG_OPCODE_MASK),
+            "EsrEl1(0x3ffc1e)"
+        );
+    }
+
+    #[test]
+    fn debug_esr_el2() {
+        assert_eq!(format!("{:?}", EsrEl2::empty()), "EsrEl2(0x0)");
+        assert_eq!(format!("{:?}", EsrEl2::IL), "EsrEl2(0x2000000)");
+        assert_eq!(
+            format!("{:?}", EsrEl2::ISS_SYSREG_OPCODE_MASK),
+            "EsrEl2(0x3ffc1e)"
+        );
+    }
+
+    #[test]
+    fn debug_esr_el3() {
+        assert_eq!(format!("{:?}", EsrEl3::empty()), "EsrEl3(0x0)");
+        assert_eq!(format!("{:?}", EsrEl3::IL), "EsrEl3(0x2000000)");
+        assert_eq!(
+            format!("{:?}", EsrEl3::ISS_SYSREG_OPCODE_MASK),
+            "EsrEl3(0x3ffc1e)"
         );
     }
 }
